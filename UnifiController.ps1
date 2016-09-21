c93b29f63a1d46d84ad4f0ca9f54a4b1f1786c6a
soveriegn3 <a.cole.nugent@gmail.com>
2016-09-21 17:21:32 -0500

    Add files via upload

---
+++ UnifiController.ps1
@@ -0,0 +1,13 @@
+﻿$VHDpath = "c:\Hyper-V\UnifiController.vhdx"
+$ISOpath = "c:\admin\ISO\ubuntu-16.04.1-server-amd64.iso"
+$VMName = "UnifiController"
+$servername = "Clark-PC"
+$VMSwitch = "External vSwitch"
+
+New-VHD -Path $VHDpath -SizeBytes 20GB -Dynamic
+New-VM -Name $VMName -MemoryStartupBytes 2048MB -Generation 1
+Add-VMHardDiskDrive -VMName $VMName -Path $VHDpath
+Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -Path $ISOpath
+Get-VMNetworkAdapter -VMName $VMName | Connect-VMNetworkAdapter -SwitchName $VMSwitch
+Start-VM -Name UnifiController
+vmconnect $servername $VMName
