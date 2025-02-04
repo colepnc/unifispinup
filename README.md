1f66250fbc30b7c11a3e432659d6f9fc9f0a07fb
colepnc <cole@pointandclicktulsa.com>
2025-02-04 12:44:18 -0600

    fixing local

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # unifispinup
+Tested on Ubuntu 20.04.1 LTS.
--- UnifiController.ps1
+++ UnifiController.ps1
@@ -1,14 +1,60 @@
-﻿$VHDpath = "c:\Hyper-V\UnifiController.vhdx"
-$ISOpath = "c:\admin\ISO\ubuntu-16.04.1-server-amd64.iso"
-$VMName = "UnifiController"
-$servername = "Clark-PC"
-$VMSwitch = "External vSwitch"
+﻿# Begin log file, this will be placed on the client the script is being run from, do not modify unless you want to disable logging
+$ErrorActionPreference = "SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path C:\admin\Unifi_log.txt -append
 
-Invoke-WebRequest "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso" -UseBasicParsing -OutFile "$ISOpath"
-New-VHD -Path $VHDpath -SizeBytes 10GB -Fixed
-New-VM -Name $VMName -MemoryStartupBytes 1024MB -Generation 1
+# Script variables, change as needed
+# If you want to run this against a remote Hyper-V host, change $ServerName to a proper computer name.
+# If you have multiple External vSwitches you'll probably also have to manually input the name of the desired vSwitch in $VMSwitch
+$ISO = "c:\admin\iso\ubuntu-24.04-live-server-amd64.iso"
+$ISOPath = "c:\admin\iso\"
+$URL = "https://releases.ubuntu.com/24.04/ubuntu-24.04-live-server-amd64.iso"
+$start_time = Get-Date
+$WebClient = New-Object System.Net.WebClient
+$VMName = "Unifi"
+$VHDpath = "c:\Hyper-V\Virtual Hard Disks\$VMName.vhdx"
+$ServerName = "$env:computername"
+$VMSwitch = Get-VMSwitch -SwitchType External |
+              Select-Object -First 1 |
+              ForEach-Object Name
+
+# Test for ISO folder existence
+If (!(Test-Path $ISOpath) -And !(Test-Path "C:\admin\isos\")) {
+New-Item -Path $ISOpath -ItemType Directory
+}
+else {
+echo "ISO directory already exists!"
+}
+
+# Download Ubuntu ISO
+If (!(Test-Path $ISO)) {
+echo "Downloading Ubuntu Server 24.04.X LTS ISO"
+$WebClient.DownloadFile($url, $ISO)
+Write-Output "Time Taken: $((Get-Date).Subtract($start_time).seconds) second(s)"
+}
+else {
+echo "Ubuntu Server 20.04.X LTS ISO already exists!"
+}
+
+# Create VHDX, VM, attach vSwitch, mount Ubuntu ISO
+New-VHD -Path $VHDpath -SizeBytes 40GB
+New-VM -Name $VMName -MemoryStartupBytes 2048MB -Generation 2
+Set-VMMemory -VMName $VMName -DynamicMemoryEnabled 0
 Add-VMHardDiskDrive -VMName $VMName -Path $VHDpath
-Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -Path $ISOpath
-Get-VMNetworkAdapter -VMName $VMName | Connect-VMNetworkAdapter -SwitchName $VMSwitch
-Start-VM -Name UnifiController
-vmconnect $servername $VMName
+Add-VMDvdDrive -VMName $VMName -Path $ISO
+if ($VMSwitch -ne $null) {
+  Get-VMNetworkAdapter -VMName $VMName |
+    Connect-VMNetworkAdapter -SwitchName $VMSwitch
+}
+$dvd = Get-VMDvdDrive -VMName $VMName
+Set-VMFirmware -VMName $VMName -EnableSecureBoot Off -FirstBootDevice $dvd
+Set-VM -Name $VMName -ProcessorCount 2 -CheckpointType Production -AutomaticStartAction Start -AutomaticCheckpointsEnabled 0 -AutomaticStopAction ShutDown
+Enable-VMIntegrationService -Name "Guest Service Interface" -VMName $VMName
+
+# Start and connect to VM
+Start-VM -Name $VMName
+vmconnect $ServerName $VMName
+
+# End log file
+Stop-Transcript
